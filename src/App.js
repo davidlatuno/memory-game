a6f7586a44a27fefcd16dcb3c48aca2be0e93b95
davidlatuno <dslatuno@gmail.com>
2018-05-09 20:52:55 -0700

    initial click score counter function

--- src/App.js
+++ src/App.js
@@ -6,14 +6,30 @@ import Header from "./components/Header"
 
 class App extends Component {
 
-    onClick = id => {
-        alert(id);
+    state = {
+        clicked: 0,
+        score: 0
+    }
+
+    onClick = input => {
+        if (input === this.state.clicked) {
+            this.setState({
+                score: 0
+            })
+        } else {
+            this.setState({
+                score: this.state.score + 1
+            })
+        }
+        this.setState ({
+            clicked: input
+        })
     }
 
     render() {
         return (
             <div className="container">
-            <Header />
+            <Header score={this.state.score}/>
             <Wrapper>
                 {Characters.map(
                     input =>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Header.css";
 
-const Header = () => 
-
-<div className="jumbotron text-center">
-    <h1 className="display-4">Memory Game</h1>
-</div>
+const Header = props =>
+    <div className="jumbotron text-center">
+        <h1 className="display-4">Memory Game</h1>
+        <p>Score: {props.score}</p>
+    </div>
 
 export default Header;
